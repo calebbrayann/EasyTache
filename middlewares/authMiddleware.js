c4f77d950e04037162367ebbd03247cc42915941
calebbrayann <melehoub@gmail.com>
2025-07-03 06:01:36 +0100

    controlle encore

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,7 +1,6 @@
 import jwt from "jsonwebtoken";
 
-export const authMiddleware = (req, res, next) => {
-  // Récupérer le token à partir des cookies ou de l'en-tête d'autorisation
+export const verifierToken = (req, res, next) => {
   const token = req.cookies?.token || req.headers.authorization?.split(" ")[1];
 
   if (!token) {
@@ -9,21 +8,10 @@ export const authMiddleware = (req, res, next) => {
   }
 
   try {
-    // Vérifier et décoder le token JWT
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-    // Vérifier que le token contient les informations nécessaires
-    if (!decoded.userId) {
-      return res.status(401).json({ error: "Jeton invalide." });
-    }
-
-    // Ajouter les informations décodées dans la requête pour les utiliser dans d'autres middlewares/routes
-    req.user = { userId: decoded.userId, role: decoded.role };
-
-    // Passer au middleware suivant
+    req.user = { userId: decoded.userId }; // Ajout de l'id utilisateur dans la requête
     next();
   } catch (error) {
-    console.error("Erreur de vérification du jeton :", error);
-    return res.status(401).json({ error: "Jeton invalide ou expiré. Veuillez vous reconnecter." });
+    return res.status(401).json({ error: "Jeton invalide ou expiré." });
   }
 };
